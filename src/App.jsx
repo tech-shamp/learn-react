5346b8f7d57417ce58453ee4bfba0b7e2f48a5e4
tech-shamp <shampytechnical@gmail.com>
2022-10-16 07:10:53 +0500

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className="heading">
+        <h1>Learn React</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis
+          commodi totam dolore dolores in eligendi incidunt voluptate odio
+          laudantium doloribus tempora ipsam cupiditate omnis, rem non iste ex
+          sit.
+        </p>
+        <a href="/">Learn More</a>
+      </div>
+    </>
+  )
+}
+
+export default App
